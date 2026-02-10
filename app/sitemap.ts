0909abf4d65de2c26cd26c69e21501e3cda500f8
LAPTOP-RS84MHN7\hasey <hase.yrsk@gmail.com>
2026-02-11 01:55:43 +0800

    fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,8 @@
 import { MetadataRoute } from 'next'
 
+// Ensure this route is statically exported for `output: export`
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://domain-tools.h2works.xyz'
 
