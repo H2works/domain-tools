f329ad515f67186204bf97723ed1e57342deab09
LAPTOP-RS84MHN7\hasey <hase.yrsk@gmail.com>
2026-02-11 01:58:04 +0800

    fix

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,8 @@
 import { MetadataRoute } from 'next'
 
+// Ensure this route is statically exported for `output: export`
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
